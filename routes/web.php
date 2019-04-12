5bf17cb7cd21066168597a1c9ba9f91813633ad4
manca_fi <manca.fi@gmail.com>
2019-04-12 15:27:33 +0800

    otentikasi user

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect(route('login'));
 });
 
-Route::resource('/kategori', 'CategoryController')->except([
-    'create', 'show'
-]);
+Auth::routes();
 
-Route::resource('/produk', 'ProductController');
+Route::group(['middleware' => 'auth'], function() {
+    Route::resource('/kategori', 'CategoryController')->except([
+        'create', 'show'
+    ]);
+    Route::resource('/produk', 'ProductController');
+    Route::get('/home', 'HomeController@index')->name('home');
+
+});
